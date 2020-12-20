7ade94bfd00a9553104e701d151cd05675ef2f83
GoDeKlein <tkachartyom51@gmail.com>
2020-12-20 13:45:41 +0300

    Видалення коментару у коді LR7

--- OP7_Tkach_110/OP7_Tkach_110.cpp
+++ OP7_Tkach_110/OP7_Tkach_110.cpp
@@ -24,28 +24,3 @@ int main()
 	system("pause");
 }
 
-/*#include <iostream>
-#include<iomanip>
-#define _USE_MATH_DEFINES
-#include "math.h"
-#include "windows.h"
-
-using namespace std;
-
-int main()
-{
-	SetConsoleCP(1251);
-	SetConsoleOutputCP(1251);
-	int m, y;
-	float Z;
-	int X = 1;
-	cout << "Введіть y: ";
-	cin >> y;
-	for (m = 1; m <= 10; m++)
-	{
-		X *= log(m * y);
-	}
-	Z = 3 * sin(y) + X;
-	cout << "Z: " << Z << endl;
-	system("pause");
-}*/
